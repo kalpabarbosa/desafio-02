61d9c7efff8949251d024445633b000716552a69
Kalpa Vrikcha <kalpabarbosa@gmail.com>
2020-06-18 14:07:30 -0300

    Implementação das rotas GET, POST e PUT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { isUuid, uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,15 +12,48 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ message: "Invalid UUID" });
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ message: `The repository with id "${id}" is not found` });
+  }
+
+  repositories[repositoryIndex] = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  return response.status(204).json({ message: "the resource has been updated" });
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -27,7 +61,6 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
 });
 
 module.exports = app;
