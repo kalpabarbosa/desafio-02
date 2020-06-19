2102b48453d9cbe2a8ebea524da5e761881fe6a4
Kalpa Vrikcha <kalpabarbosa@gmail.com>
2020-06-18 21:45:11 -0300

    Criação dos middlewares e implementação da rota de likes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,47 @@ const express = require("express");
 const cors = require("cors");
 const { isUuid, uuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository ID.' });
+  }
+
+  return next();
+}
+
+function notFound(request, response, next) {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ message: `Repository not found` });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/repositories/:id', validateRepositoryId, notFound);
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -32,20 +65,12 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  const { title, url, techs } = request.body;
   const { id } = request.params;
-
-  if(!isUuid(id)) {
-    return response.status(400).json({ message: "Invalid UUID" });
-  }
+  const { title, url, techs } = request.body;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if(repositoryIndex < 0) {
-    return response.status(400).json({ message: `The repository with id "${id}" is not found` });
-  }
-
-  repositories[repositoryIndex] = {
+  repository = {
     id,
     title,
     url,
@@ -53,21 +78,15 @@ app.put("/repositories/:id", (request, response) => {
     likes: repositories[repositoryIndex].likes
   };
 
-  return response.status(204).json({ message: "the resource has been updated" });
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  if(!isUuid(id)) {
-    return response.status(400).json({ message: "Invalid UUID" });
-  }
-
   const repository = repositories.find(repository => repository.id === id);
-
-  if(!repository) {
-    return response.status(404).json({ message: "Repository not found" });
-  }
 
   repositories.pop(repository);
 
@@ -75,6 +94,13 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  repositories[repositoryIndex].likes++;
+
+  return response.json({ likes: repositories[repositoryIndex].likes });
 });
 
 module.exports = app;
